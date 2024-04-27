c9327e33ee03058cdc4e5ee3909515205f99b7a0
Danil Zhukov <zhukov.danila2004@yandex.ru>
2024-04-28 04:39:40 +0700

    fixed fizzbuzz

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -4,7 +4,7 @@ numbers=$1
 
 for ((i=1; i<=numbers; i++)); do
 	if ((i % 3 == 0)) && ((i % 5 == 0)); then
-		echo "Fiiz Buzz"
+		echo "Fizz Buzz"
 	elif ((i % 5 == 0)); then
 		echo "Buzz"
 	elif ((i % 3 == 0)); then
